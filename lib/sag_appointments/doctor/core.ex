81fad0a08942e4d15924f568b0f3b6dd47d08dd0
Piotr Żelazko <piotr.zelazko@icloud.com>
2020-01-24 00:30:21 +0100

    add: doctor core

---
+++ lib/sag_appointments/doctor/core.ex
@@ -0,0 +1,35 @@
+defmodule SagAppointments.Doctor.Core do
+  alias Timex.Interval
+  alias Timex.Duration
+
+  alias SagAppointments.Doctor
+
+  def slots(%Doctor{} = state, begin_date), do: slots(state, begin_date, begin_date)
+
+  def slots(%Doctor{working_hours: working_hours, visit_time: visit_time}, begin_date, end_date) do
+    query_end_time = Timex.to_naive_datetime(end_date) |> Timex.shift(hours: 23, minutes: 59)
+
+    Interval.new(from: begin_date, until: query_end_time)
+    |> Enum.map(&day_intervals(working_hours, visit_time, &1))
+    |> Enum.map(&Enum.to_list/1)
+    |> List.flatten()
+  end
+
+  def available_slots(%Doctor{} = state, taken, begin_date),
+    do: available_slots(state, taken, begin_date, begin_date)
+
+  def available_slots(%Doctor{} = state, taken, begin_date, end_date) do
+    slots(state, begin_date, end_date) -- taken
+  end
+
+  defp day_intervals(working_hours, visit_time, date) do
+    with {:ok, %{start: day_start, end: day_end}} <-
+           Map.fetch(working_hours, Timex.weekday(date)),
+         from <- Timex.add(date, Duration.from_hours(day_start)),
+         until <- Timex.add(date, Duration.from_hours(day_end)) do
+      Interval.new(from: from, until: until, step: [minutes: visit_time])
+    else
+      _ -> []
+    end
+  end
+end
---
+++ test/doctor/core_test.exs
@@ -0,0 +1,42 @@
+defmodule SagAppointments.Doctor.CoreTest do
+  use ExUnitFixtures
+  use ExUnit.Case, async: true
+  use Timex
+
+  alias SagAppointments.Doctor
+  import SagAppointments.Doctor.Core
+
+  @default_days %{1 => %{start: 10, end: 17}, 3 => %{start: 14, end: 15}}
+  @monday Timex.shift(Timex.zero(), days: 2)
+
+  deffixture init_state() do
+    struct!(Doctor,
+      name: "John",
+      surname: "Smith",
+      clinic: "XYZ",
+      working_hours: @default_days,
+      visit_time: 10
+    )
+  end
+
+  @tag fixtures: [:init_state]
+  test "computes slots for one day", %{init_state: state} do
+    assert length(slots(state, @monday)) == 7 * 6
+    assert slots(state, Timex.shift(@monday, days: 1)) == []
+    assert length(slots(state, Timex.shift(@monday, days: 2))) == 6
+  end
+
+  @tag fixtures: [:init_state]
+  test "computes available slots", %{init_state: state} do
+    [h | tail] = slots(state, @monday)
+    taken_slots = [h]
+
+    assert available_slots(state, taken_slots, @monday) == tail
+
+    assert available_slots(state, taken_slots, Timex.shift(@monday, days: 1)) ==
+             slots(state, Timex.shift(@monday, days: 1))
+
+    assert available_slots(state, taken_slots, Timex.shift(@monday, days: 2)) ==
+             slots(state, Timex.shift(@monday, days: 2))
+  end
+end
