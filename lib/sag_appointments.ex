464396ae24da7278d75fa640bea4701e85dd5a06
Piotr Żelazko <piotr.zelazko@icloud.com>
2020-01-24 00:28:20 +0100

    feat: add project entry files

---
+++ lib/sag_appointments.ex
@@ -0,0 +1,2 @@
+defmodule SagAppointments do
+end
---
+++ lib/sag_appointments/application.ex
@@ -0,0 +1,9 @@
+defmodule SagAppointments.Application do
+  use Application
+
+  def start(_type, _args) do
+    children = []
+
+    Supervisor.start_link(children, strategy: :one_for_one, name: SagAppointments.Supervisor)
+  end
+end
