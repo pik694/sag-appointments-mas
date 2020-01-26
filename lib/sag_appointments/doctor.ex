d359567bcfae7d9693870364ecf616a50d2db140
Piotr Żelazko <piotr.zelazko@icloud.com>
2020-01-26 14:29:10 +0100

    feat: add doctor specialisation

--- lib/sag_appointments/doctor.ex
+++ lib/sag_appointments/doctor.ex
@@ -4,11 +4,13 @@ defmodule SagAppointments.Doctor do
   @default_working_hours Enum.map(1..5, fn weekday -> {weekday, %{start: 9, end: 17}} end)
                          |> Map.new()
 
-  defstruct [:name, :surname, :clinic, :working_hours, :visit_time]
+  defstruct [:name, :surname, :field, :clinic, :working_hours, :visit_time]
 
   def init(opts) do
     default_values = [working_hours: @default_working_hours, visit_time: 15]
     state = struct!(__MODULE__, Keyword.merge(default_values, opts))
     {:ok, state}
   end
+       
+
 end
