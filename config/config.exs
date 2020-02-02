0f623dafe3b70a998b132213937caf83350f9b2d
Piotr Żelazko <piotr.zelazko@icloud.com>
2020-02-02 19:12:45 +0100

    feat: add random crashes to doctor process

--- config/config.exs
+++ config/config.exs
@@ -5,6 +5,12 @@ config :logger,
        format: "$time $metadata[$level]: $message\n",
        metadata: [:requiest_id]
 
+config :logger,
+  level: :warn
+
 # config :phoenix, :json_library, Jason
+
+config :sag_appointments,
+  doctor_crash_threshold: 0
 
 import_config "#{Mix.env()}.exs"
